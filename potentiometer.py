a14719b6555dd57d345438fd18a872d293e3b579
Lee Bottomley <155629288+LeeJBott@users.noreply.github.com>
2024-02-16 11:07:49 +0000

    Update potentiometer.py

--- potentiometer.py
+++ potentiometer.py
@@ -1,13 +1,13 @@
-import machine
+import machine # Raspberry Pi Pico.
 from time import sleep
 p1 = 28 #Potentiometer middle pin
 potent = machine.ADC(p1) #class potent, machine, Analogue to Digital Converter
 
 while True:
-    potVal = potent.read_u16() #read_u16 = reading 16 bit number (2 x^y raised to power of 16 = 65535)
+    potVal = potent.read_u16() #read_u16 = reading 16 bit number (2 x^y raised to power of 16 = 65535) - (See MathematicalWorkings.PNG)
     print(potVal)
     sleep(1)
     potVal = potent.read_u16()
-    voltage=(3.3/65106)*potVal-(400*3.3/65106) #determining the equation of a line(slope)
+    voltage=(3.3/65106)*potVal-(400*3.3/65106) #determining the equation of a line(slope) - ( See MathematicalWorkings.PNG )
     print(voltage)
     sleep(1)
